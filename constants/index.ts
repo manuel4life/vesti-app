13f7f4f7303dc8dd14942812af8688f04e78e3e0
manuel4life <allotey4eva@gmail.com>
2025-09-20 13:14:58 +0200

    added images

--- constants/index.ts
+++ constants/index.ts
@@ -1,2 +1,4 @@
 export const HERO: string = "/images/hero.jpg";
 export const MAIN: string = "/images/main.png";
+export const MALE: string = "/images/male.jpg";
+export const FEMALE: string = "/images/female.jpg";
