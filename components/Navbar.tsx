148366fe9bd77eb4d04d8393a048e94e9be91b2c
manuel4life <allotey4eva@gmail.com>
2025-09-26 18:58:50 +0200

    Moblie toggle

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -84,7 +84,7 @@ export default function Navbar() {
               Home
             </Link>
             <Link
-              href="/products"
+              href="/"
               className="block px-3 py-2 text-gray-700 hover:bg-gray-100 rounded"
             >
               Products
